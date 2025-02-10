7d5127ef296136bdb9c4ada81bf60519e5268a5b
christian <christianbiringanine22@gmail.com>
2025-02-10 10:46:59 +0200

    set github flow

---
+++ src/app/home/homePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const HomePage = () => {
+  return (
+    <div>
+      <p className="text-xl">Home page will go here</p>
+    </div>
+  );
+};
+
+export default HomePage;
