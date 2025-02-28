58506213d8e1056d7e1e82a6f76c722c0bce305c
christian <christianbiringanine22@gmail.com>
2025-02-28 19:49:22 +0200

    Readjust hero section

--- src/app/components/heroSection.tsx
+++ src/app/components/heroSection.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import { Phone } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -7,10 +5,10 @@ import Link from "next/link";
 
 export default function HeroSection() {
   return (
-    <section className="w-[90%] m-auto bg-gray-50 py-8 md:py-8">
+    <section className="w-[90%] mx-auto bg-gray-50 py-12 md:py-24">
       <div className="container px-4 md:px-6">
-        <div className="flex flex-col-reverse items-center justify-center gap-6 lg:flex-row-reverse lg:gap-6">
-          <div className="flex flex-col justify-center space-y-4 ">
+        <div className="grid items-center gap-6 lg:grid-cols-2 lg:gap-12">
+          <div className="flex flex-col justify-center space-y-4">
             <div className="space-y-2">
               <h1 className="text-4xl font-bold tracking-tighter text-[#2a3990] sm:text-5xl xl:text-6xl/none">
                 We Code
@@ -35,13 +33,13 @@ export default function HeroSection() {
                   className="mt-4 bg-[#2a3990] hover:bg-[#1e2a6e] text-white"
                   size="lg"
                 >
-                  <Link href="/#contact">Get in touch</Link>
+                  Get in touch
                 </Button>
               </div>
             </div>
           </div>
           <div className="flex justify-center lg:justify-end">
-            <div className="h-fit">
+            <div className="relative h-[350px] w-[350px] sm:h-[400px] sm:w-[400px] md:h-[500px] md:w-[500px]">
               <Image
                 src="/hero.svg"
                 alt="Developer working at desk with plants and computer"
